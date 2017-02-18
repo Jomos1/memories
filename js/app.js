00677c49f913ebbbf6d8263722a13e92bca97e81
Student <idk123@gmail.com>
2017-02-17 15:32:12 -1000

    new file:   assets/bg.jpg 	new file:   assets/img3.png 	deleted:    css/style.css 	new file:   css/styles.css 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,76 @@
+var memory_array = ['1','1','2','2','3','3'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
+function newBoard(){
+	tiles_flipped = 0;
+	var output = '';
+    memory_array.memory_tile_shuffle();
+	for(var i = 0; i < memory_array.length; i++){
+		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+	}
+	document.getElementById('memory_board').innerHTML = output;
+}
+function memoryFlipTile(tile,val){
+	if(tile.innerHTML == "" && memory_values.length < 2){
+		tile.style.background = '#dae1ed';
+		tile.innerHTML = val;
+		if(memory_values.length == 0){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+		} else if(memory_values.length == 1){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+			if(memory_values[0] == memory_values[1]){
+				tiles_flipped += 2;
+				// Clear both arrays
+				memory_values = [];
+            	memory_tile_ids = [];
+				// Check to see if the whole board is cleared
+				if(tiles_flipped == memory_array.length){
+					alert("Congratulations. You aren't a disappointment!");
+					document.getElementById('memory_board').innerHTML = "";
+					newBoard();
+				}
+			} else {
+				function flip2Back(){
+				    // Flip the 2 tiles back over
+				    var tile_1 = document.getElementById(memory_tile_ids[0]);
+				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
+            	    tile_1.innerHTML = "";
+				    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
+            	    tile_2.innerHTML = "";
+				    // Clear both arrays
+				    memory_values = [];
+            	    memory_tile_ids = [];
+				}
+				setTimeout(flip2Back, 700);
+			}
+		}
+	}
+}
+// Timer
  var timer = function(){
    var counter = 60;
    setInterval(function() {
      counter--;
-      if (counter >= 0) {
+      if (counter >= 0 && ) {
          span = document.getElementById("time");
          span.innerHTML = counter;
-      }
+      } //Checking if timer hits zero 
       if (counter === 0) {
-         alert('you suck');
+         alert('Much disappointment...');
          clearInterval(counter);
        }
      }, 1000);
 };
-
-var init = function() {
-  var card = document.getElementById('card');
-  
-  document.getElementById('flip').addEventListener( 'click', function(){
-    card.toggleClassName('flipped');
-  }, false);
-};
-
-window.addEventListener('DOMContentLoaded', init, false);
